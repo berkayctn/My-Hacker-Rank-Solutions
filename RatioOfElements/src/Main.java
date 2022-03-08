d94af356d7b8b06f4fd449553edb3e7c665866d6
Berkay Cetin <berkaycetin1710@gmail.com>
2022-03-09 00:09:24 +0300

    Done !

---
+++ RatioOfElements/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> arr = new ArrayList();
+        arr.add(-4);
+        arr.add(3);
+        arr.add(-9);
+        arr.add(0);
+        arr.add(4);
+        arr.add(1);
+
+        plusMinus(arr);
+
+
+    }
+
+    public static void plusMinus(List<Integer> arr) {
+
+        int zero = 0;
+        int positive = 0;
+        int negative = 0;
+
+
+        for (int i = 0; i < arr.size(); i++) {
+
+            if (arr.get(i) < 0) {
+                negative++;
+
+            } else if (arr.get(i) == 0) {
+                zero++;
+
+            } else
+                positive++;
+        }
+
+        System.out.printf("proportion of positive values: %.6f %n", ((double) positive / arr.size()));
+        System.out.printf("proportion of negative  values: %.6f %n", ((double) negative / arr.size()));
+        System.out.printf("proportion of zeros : %.6f %n", ((double) zero / arr.size()));
+
+        System.out.println();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
