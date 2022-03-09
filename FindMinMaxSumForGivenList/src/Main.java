fd88981da1f2ccc737d62f5a17ac517aed43dc98
Berkay Cetin <69761460+berkayctn@users.noreply.github.com>
2022-03-09 22:23:11 +0300

    Done !

--- FindMinMaxSumForGivenList/src/Main.java
+++ FindMinMaxSumForGivenList/src/Main.java
@@ -16,7 +16,13 @@ public class Main {
         miniMaxSum(arr);
 
     }
+    
+    /*    
+    Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+    Then print the respective minimum and maximum values as a single line of two space-separated long integers.    
+    */
 
+    //This funciton solves that problem for more than five positive integers
     public static void miniMaxSum(List<Integer> arr) {
 
         long sum = 0 , temp;
