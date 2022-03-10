fe896851388a0170f5cea4e4ff4479d7751a0d3a
Berkay Cetin <69761460+berkayctn@users.noreply.github.com>
2022-03-10 23:26:40 +0300

    Done !

--- JustForFun/RandomStringGenerator/src/Main.java
+++ JustForFun/RandomStringGenerator/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
         //This code piece generates a 25 digits random string using ASCII codes
 
-        char [] arr = new char[25];
+         char[] arr = new char[25];
 
-        for(int i = 0 ; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             double randomNumber = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
-            char convertedChar = (char)randomNumber;
+            char convertedChar = (char) randomNumber;
             arr[i] = convertedChar;
             System.out.print(arr[i]);
         }
