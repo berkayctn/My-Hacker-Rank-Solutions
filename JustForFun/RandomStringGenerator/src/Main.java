e546bf43d4d0b2dfa69e9603a6813d19f2f22cdb
Berkay Cetin <69761460+berkayctn@users.noreply.github.com>
2022-03-10 23:45:16 +0300

    Done !

--- JustForFun/RandomStringGenerator/src/Main.java
+++ JustForFun/RandomStringGenerator/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //This code piece generates a 25 digits random string using ASCII codes
+        //This code piece generates a 50 digits random string using ASCII codes
 
-         char[] arr = new char[25];
+         char[] arr = new char[50];
 
         for (int i = 0; i < arr.length; i++) {
             double randomNumber = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
